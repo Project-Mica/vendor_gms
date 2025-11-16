06d149a02c69f52f054ea0b7a1ff65239423f884
aswin7469 <aswinas@pixysos.com>
2025-11-17 02:58:09 +0530

    gms: add a basic script to generate proprietary files
    * BuildBOt friendly

---
+++ generate-gms.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+FILE_URL="https://dl.aswinaskurup.xyz/0:/Pixel-Dumps/mustang-BD3A.251105.010.E1-dump.7z"
+
+echo "[*] Downloading latest pixel Dump"
+curl -L "$FILE_URL" -o "dump.7z"
+
+echo "[*] Extracting..."
+7z x "dump.7z" -o"pixel-dump" >/dev/null
+
+echo "[*] Running extract-utils"
+bash extract-files.sh "pixel-dump"
+
+echo "[*] Cleaning up..."
+rm "dump.7z"
+rm -rf "pixel-dump"
+# run git reset to ensure overlays arent modified
+git reset --hard
+
+echo "[+] Done Generating GMS you can build now"
